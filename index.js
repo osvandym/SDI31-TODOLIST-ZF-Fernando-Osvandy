2934410113cc25ccf855883eb0c52f529f9bf5d0
yepitsaname <fernando.g.berry@gmail.com>
2025-07-09 15:42:30 -0400

    Creates a create_NewTask() function and shows another method of doing it

--- index.js
+++ index.js
@@ -1,38 +1,96 @@
 /*
-•Create of list of tasks
- -unordered list
-  -Stretch Goal: option to toggle between ordered/unordered
+=========CREATING A TASK FUNCTIONALITY=========
+• Submit the new task row
+  - Submit button click occurs
+  - Grab task id: new-task
+  - Grab date id: new-date
+  - Create row id
+  - Create our html with row ID and task & date inputs
+  - Increment row counter
+  - Create <tr> element
+  - Set <tr> innerHtml with earlier created html
+  - Insert new task at the top of the list
 
-•Add to the list
-•Strikethrough individual list items
-•Remove from the list
+• Clear the new task row
+  - Clear button click occurs
+  - Grab task id: new-task
+  - Grab date id: new-date
+  - Set values to default
 
-Style
-•Div for page
- -H1 title To Do List -> Possibly encapsulate in <header></header>
- -Table
-  -Table Header
-   -Check When Completed, Task, Deadline, Delete Item
-  -Table Body
-   -Each Table Row needs to have the following:
-    -Interactive check box - when checked strikethrough task
-    -td for the task
-    -td for the deadline -> date box
-    -Interactive element to delete parent table row
-      -Stretch Goal One: Confirm Deletion
-      -Stretch Goal Two: Archive completed tasks in a viewable location
-   -On task fill out task and submit, create new task line
+========TASK FUNCTIONALITY=========
+• Strikethrough 'completed' tasks
+  - Event Check box click occurs
+    - POSSIBLY: Check the check box state
+    - POSSIBLY: Check the task classes for completed
+    - POSSIBLY: If has completed && checkbox is unchecked, remove completed otherwise do nothing
+    - POSSIBLY: If does not have completed && checkbox is checked, add completed
+  - Get next sibling
+    OPTIONS TO GRAB CORRECT TABLE ROW
+      this.nexSibling
+      document.querySelector('#someID')
+  - Toggle completed class on that sibling
 
-•Stretch Goal: save data -> session storage or local storage > take our data and transform it into JSON
-  then store it with a key in session or local storage. Run windows.onload() > to check for the key in storage and if its there
-  transform the JSON into HTML or data and then put on the page.
+• Delete tasks
+  - On image click occurs
+  - Get parent <tr> by ID
+    OPTIONS TO GRAB CORRECT TABLE ROW
+      this.parentElement.parentElement
+      document.querySelector('#someID')
+  - Delete the parent <>
+
+  ========STRETCH GOALS==========
+• Stretch: Store Task Data
+• Stretch: Archive completed tasks
+
+
 */
 
-let task_html = `
-        <tr>
+
+const create_NewTask = (() => {
+  var rowIdCount = 0;
+
+  return () => {
+    // Grabs task and date data
+    let newTask = document.querySelector("#new-task").value;
+    let newDate = document.querySelector("#new-date").value;
+
+    // Creates <tr> and assigns an id and innerHTML
+    let task = document.createElement("tr");
+    task.id = `row-${rowIdCount}`  // Option 1: "row-" + rowIdCount; or Option 2: `row-${rowIdCount}`
+    task.innerHTML = `
           <td><input type="checkbox"/></td>
-          <td>Grocery Shopping</td>
-          <td><input type="date"/></td>
+          <td>${newTask}</td>
+          <td><input type="date" value="${newDate}"/></td>
           <td><input type="image" alt="delete" src="/img/delete-icon.png" width="48" height="48"/></td>
-        </tr>
-`
+    `;
+
+    // Appends <tr> to <tbody> and moves it after the task creation row
+    document.querySelector("#create-task").after(task);
+
+    rowIdCount++ // Options: rowIdCount = rowIdCount + 1; rowIdCount += 1; rowIdCount++; ++rowIdCount; BUT NOT rowIdCount =+ 1;
+  };
+})();
+
+// var rowIdCount = 0;
+
+// function create_NewTask(){
+//   // Grabs task and date data
+//   let newTask = document.querySelector("#new-task").value;
+//   let newDate = document.querySelector("#new-date").value;
+
+//   // Creates <tr> and assigns an id and innerHTML
+//   let task = document.createElement("tr");
+//   task.id = `row-${rowIdCount}`  // Option 1: "row-" + rowIdCount; or Option 2: `row-${rowIdCount}`
+//   task.innerHTML = `
+//         <td><input type="checkbox"/></td>
+//         <td>${newTask}</td>
+//         <td><input type="date" value="${newDate}"/></td>
+//         <td><input type="image" alt="delete" src="/img/delete-icon.png" width="48" height="48"/></td>
+//   `;
+
+//   // Appends <tr> to <tbody> and moves it after the task creation row
+//   document.querySelector("#create-task").after(task);
+
+//   rowIdCount++ // Options: rowIdCount = rowIdCount + 1; rowIdCount += 1; rowIdCount++; ++rowIdCount; BUT NOT rowIdCount =+ 1;
+// };
+
